31d3d7ef1852d4ddf4ba41a76a4fbd23355aa1a8
lucy <lucy.permata@aia.com>
2021-09-19 01:25:45 +0700

    change to swift

--- www/cyplugin.js
+++ www/cyplugin.js
@@ -3,3 +3,17 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'cyplugin', 'coolMethod', [arg0]);
 };
+
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'cyplugin', 'coolMethod', [arg0]);
+};
+
+module.exports.add = function (arg0, success, error) {
+    exec(success, error, 'cyplugin', 'add', [arg0]);
+};
+
+module.exports.getUserData = function (success, error) {
+    exec(success, error, 'cyplugin', 'getUserData', []);
+};
