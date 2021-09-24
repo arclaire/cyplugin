fd0346bf105d949fc991b0a98a1f8d6f2decc173
lucy <lucy.permata@aia.com>
2021-09-24 14:11:57 +0700

    update

--- www/cyplugin.js
+++ www/cyplugin.js
@@ -4,14 +4,12 @@ exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'cyplugin', 'coolMethod', [arg0]);
 };
 
-var exec = require('cordova/exec');
-
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'cyplugin', 'coolMethod', [arg0]);
 };
 
 module.exports.add = function (arg0, success, error) {
-    exec(success, error, 'cyplugin', 'add', [arg0]);
+    exec(success, null, 'cyplugin', 'add', [arg0]);
 };
 
 module.exports.getUserData = function (success, error) {
