b14b1e8c5ed21e692dde5177825a44417ccb1af4
lucy <lucy.permata@aia.com>
2021-09-29 15:26:27 +0700

    change functiom

--- www/cyplugin.js
+++ www/cyplugin.js
@@ -4,12 +4,8 @@ exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'cyplugin', 'coolMethod', [arg0]);
 };
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'cyplugin', 'coolMethod', [arg0]);
-};
-
-module.exports.add = function (arg0, success, error) {
-    exec(success, null, 'cyplugin', 'add', [arg0]);
+module.exports.add = function (arg0, adddsuccess, error) {
+    exec(addsuccess, adderror, 'cyplugin', 'add', [arg0]);
 };
 
 module.exports.getUserData = function (success, error) {
