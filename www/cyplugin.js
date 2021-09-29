5987e8916fd1a5ac384214af6ff07f3650cf1c1c
lucy <lucy.permata@aia.com>
2021-09-29 16:12:11 +0700

    change plugin method

--- www/cyplugin.js
+++ www/cyplugin.js
@@ -1,13 +1,33 @@
+// var exec = require('cordova/exec');
+
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'cyplugin', 'coolMethod', [arg0]);
+// };
+
+// module.exports.add = function (arg0,  adddsuccess, error) {
+//     exec(function addsuccess(){}, function adderror(){}, 'cyplugin', 'add', [arg0]);
+// };
+
+// module.exports.getUserData = function (success, error) {
+//     exec(success, error, 'cyplugin', 'getUserData', []);
+// };
+
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'cyplugin', 'coolMethod', [arg0]);
+var PLUGIN_NAME = "cyplugin"; // This is just for code completion uses.
+
+var YourPluginName = function() {}; 
+
+YourPluginName.coolMethod = function(onSuccess, onError) {
+   exec(onSuccess, onError, PLUGIN_NAME, "coolMethod", [arg0]);
 };
 
-module.exports.add = function (arg0, adddsuccess, error) {
-    exec(addsuccess, adderror, 'cyplugin', 'add', [arg0]);
-};
+YourPluginName.add = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "add", [arg0]);
+ };
 
-module.exports.getUserData = function (success, error) {
-    exec(success, error, 'cyplugin', 'getUserData', []);
-};
+ YourPluginName.add = function(onSuccess, onError) {
+    exec(onSuccess, onError, getUserData, "add", []);
+ };
+
+module.exports = YourPluginName;
