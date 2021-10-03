9db82e0856a4d95f57dafda5ace9f06b45e2cc2f
lucy <lucy.permata@aia.com>
2021-10-04 02:49:00 +0700

    kkkk

--- www/cyplugin.js
+++ www/cyplugin.js
@@ -16,12 +16,13 @@
 
 module.exports = {
     add: function (arg0, successCallback, errorCallback) {
-        cordova.exec(function successCallback(){}, function errorCallback(){}, "cyplugin", "add", [arg0]);
+        cordova.exec(successCallback,  errorCallback, "cyplugin", "add", [arg0]);
     },
     coolMethod: function (arg0, successCallback, errorCallback) {
-        cordova.exec(function successCallback(){}, function errorCallback(){}, "cyplugin", "coolMethod", [arg0]);
+        cordova.exec(successCallback,  errorCallback, "cyplugin", "coolMethod", [arg0]);
     },
     getUserData: function (arg0, successCallback, errorCallback) {
-        cordova.exec(function successCallback(){}, function errorCallback(){}, "cyplugin", "getUserData", []);
+        cordova.exec(successCallback,  errorCallback, "cyplugin", "getUserData", []);
     }
 };
+
